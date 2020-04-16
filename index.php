20ecd386670cc811ca8a25386ee09c399aa62db7
Vimbayinashe <vimbayinashemandaza@gmail.com>
2020-04-16 23:24:48 +0200

    Improve Form

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ include_once "private.php";
 
 $dbh = new PDO('mysql:host=localhost;dbname=zoo;charset=UTF8', $user, $password);
 
+//select all animals
 $queryAll = "SELECT * FROM animals";
 
 $statementAll = $dbh->prepare($queryAll, array(PDO::FETCH_ASSOC));    
@@ -21,8 +22,44 @@ $allAnimals = $statementAll->fetchAll();
 // echo "<br>";
 // echo "</pre>";
 
+//get input from user's search
+if(isset($_POST['search'])) {
+    $menuInput = $_POST['animal'];
+    $textInput = $_POST['userText'];
+    // echo $menuInput;
+    // echo $textInput;
 
-//select by name
+    if(isset($_POST['userText'])) {
+        $textInput = cleanData($_POST['userText']);   
+        
+        // check if text input only contains letters and whitespace
+        if (!preg_match("/^[a-zA-ZåäöÅÄÖ ]*$/",$textInput)) {
+            echo "incorrect text syntax";
+            $syntaxError = "ange giltigt namn format";
+        } else {
+            $syntaxError = "";
+            echo $textInput;
+        }     
+    } else {
+        $textInput = "";
+    }
+
+    if(isset($_POST['animal'])){
+        $menuInput = $_POST['animal'];
+        echo $menuInput;
+    }
+}
+
+//a function to sanitise user text input
+function cleanData ($data) {
+    $data = trim($data);        
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);    
+    return $data;
+}
+
+
+//select an animal from drop-down menu
 $query = "SELECT * FROM animals where name = ?";
 
 $statement = $dbh->prepare($query, array(PDO::FETCH_ASSOC));  
@@ -35,7 +72,7 @@ echo "<pre>";
 var_dump($selection);
 echo "</pre>";
 
-
+//select an animal from input box
 $query = "SELECT * FROM animals where name like :name";
 
 $statement = $dbh->prepare($query, array(PDO::FETCH_ASSOC));  
@@ -60,15 +97,29 @@ echo "</pre>";
 <body>
     <h1></h1>
 
-    <label for="animal">Välj ett djur:</label>
-    <select name="animal" id="animal">
-    <option value="">  </option>
-    <?php
-        foreach ($allAnimals as $animal) {
-            echo "<option value =".$animal['id'].">".$animal['name']."</option>";
-        } 
-    ?>
-    </select>
+
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"     
+        method="post"
+    >
+        <label for="animal">Välj ett djur:</label>
+        <select name="animal" id="animal">
+        <option value="">  -- välj -- </option>
+        <?php
+            foreach ($allAnimals as $animal) {
+                echo "<option>".$animal['name']."</option>";
+            } 
+            ?>
+        </select>
+
+        <label for="search">Sök efter namn: </label>
+        <input type="text" name="userText">
+        <div>
+            <input name="search" type="submit">        
+        </div>
+        
+    </form>
+    <!-- <p><?php echo $success; ?></p> -->
+
 
 </body>
 </html>
